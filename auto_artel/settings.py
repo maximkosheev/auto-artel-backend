f08d435b90fcd53945b5a5bdf368939ae694d39f
mkoscheev <mkoscheev@alfabank.ru>
2025-07-24 11:55:33 +0500

    Подключил postgresql и настроил автоматическое создание групп и пользователя auto_artel_bot

--- auto_artel/settings.py
+++ auto_artel/settings.py
@@ -79,16 +79,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'auto_artel.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "OPTIONS": {
+            "service": "auto_artel",
+            "passfile": ".pgpass",
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- home/apps.py
+++ home/apps.py
@@ -1,6 +1,30 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
+
+    def ready(self):
+        post_migrate.connect(self.initialize, sender=self)
+
+    def initialize(self, **kwargs):
+        from django.contrib.auth.models import User, Group
+
+        def create_groups():
+            api_group = Group(name='api')
+            manager_group = Group(name='manager')
+            client_group = Group(name='client')
+
+            Group.objects.bulk_create(objs=[api_group, manager_group, client_group], ignore_conflicts=True)
+
+        def create_api_user():
+            api_group = Group.objects.get(name='api')
+            if not User.objects.filter(username='auto_artel_bot').exists():
+                api_user = User.objects.create_user(username='auto_artel_bot', email=None, password='xxx')
+                api_user.groups.add(api_group)
+                api_user.save()
+
+        create_groups()
+        create_api_user()
