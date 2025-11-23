93a6448fb59f6972439fccb3f33e35fc07b3fccf
maximkosheev <maximkosheev@gmail.com>
2025-11-24 00:23:47 +0500

    верстка страницы клиентов в модуле chat

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
+from django.db.models import Count, Q, Max
+from django.shortcuts import render
 from django.views import generic
+
+from orders.models import Client
 
 
 def is_manager(user):
@@ -11,9 +15,24 @@ class IsManagerMixin(UserPassesTestMixin):
         return is_manager(self.request.user)
 
 
-# Create your views here.
-class ChatListView(IsManagerMixin, generic.ListView):
-    pass
+class ChatListView(IsManagerMixin, generic.TemplateView):
+    template_name = "chat/clients.html"
+
+    def get(self, request, *args, **kwargs):
+        client_name = request.GET.get('search')
+        where = Q(name__icontains=client_name) if client_name is not None else Q()
+
+        clients = (Client.objects.annotate(
+            unread_count=Count(
+                'chatmessage',
+                filter=Q(chatmessage__manager_id__isnull=True, chatmessage__viewed=False)),
+            last_message_time=Max('chatmessage__created'))
+                   .filter(where)
+                   .order_by('-unread_count', '-last_message_time'))
+        fake_clients = [clients[0]] * 100
+        return render(request, self.template_name, {
+            'clients': fake_clients
+        })
 
 
 class ChatWithClient(IsManagerMixin, generic.DetailView):
